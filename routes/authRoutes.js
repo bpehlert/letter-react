d1b198bcffa193b3717c76c897c47845b6eeb55f
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-12-22 11:30:42 -0700

    finished up auth and config

---
+++ routes/authRoutes.js
@@ -0,0 +1,23 @@
+const passport = require("passport");
+
+// to use route handlers, we export the routes as the body of a function,
+// that is immidiately exported. This function is then ran through the index.js file.
+module.exports = app => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
+
+  app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+};
---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); //null is the error
+});
+
+// Pulls userId from the cookie
+passport.deserializeUser((userId, done) => {
+  User.findById(userId).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    //  Callback function to save user to DB
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
