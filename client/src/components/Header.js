31f94fc61bd8645dd399c5bf3ac1573e60346489
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-12-22 22:00:21 -0700

    test commit to github

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+// Test push to github
 
 class Header extends Component {
   renderContent() {
