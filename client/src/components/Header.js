08458f02004919b0e57440f06bd3d9d5adc2a6fb
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-12-24 11:53:17 -0700

    production to heroku

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-// Test push to github
 
 class Header extends Component {
   renderContent() {
