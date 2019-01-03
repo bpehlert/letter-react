1d000d0cee4e01221d059470564635a405f4ab0e
Benjamin Ehlert <behlert@Benjamins-MacBook-Pro.local>
2019-01-02 21:38:01 -0700

    Test commit to github

---
+++ models/Test.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose; // destructured from const Schema = mongoose.Schema
+
+const testSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model("test", testSchema);
