dde87e500fd5508f7dfc224ce4d5746642ff9e26
Ben <benjamin.ehlert@gmail.com>
2019-01-07 12:22:23 -0700

    new route handler for entries

--- routes/entryRoutes.js
+++ routes/entryRoutes.js
@@ -1,7 +1,22 @@
+const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 
+const Entry = mongoose.model("entries"); //This is how you require in mongoose mondel classes
+
 module.exports = app => {
-  app.post("/api/entries", (req, res) => {
-    //   Check to see if a user is logged in
+  app.post("/api/entries", requireLogin, (req, res) => {
+    const { date, body, entryNumber, collaborators } = req.body;
+
+    const entry = new Entry({
+      date,
+      body,
+      entryNumber,
+      collaborators: collaborators
+        .split(",")
+        .map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateCreated: Date.now(),
+      lastEdited: Date.now()
+    });
   });
 };
