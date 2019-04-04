081df66a751f726f07e53d9e782af538937f24ef
Benjamin Ehlert <benjamin.ehlert@gmail.com>
2019-04-04 09:12:37 -0600

    Small change to account component.

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Account extends Component {
-  state = { props: this.props };
-
   render() {
     const { name, email, entries } = this.props.auth || "";
     const firstName = this.props.auth ? name.firstName : "";
     const lastName = this.props.auth ? name.lastName : "";
+
     return (
       <div>
         <h1>Account Settings</h1>
