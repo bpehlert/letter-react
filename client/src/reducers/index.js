bb66afe62df4971d81b1b7bb127900d5b13560a5
Benjamin Ehlert <benjamin.ehlert@gmail.com>
2019-06-18 14:57:10 -0600

    added entries reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import entryReducer from "./entryReducer";
+import entriesReducer from "./entriesReducer";
 
 export default combineReducers({
   auth: authReducer,
-  entry: entryReducer
+  entry: entryReducer,
+  entries: entriesReducer
 });
